60628d46638212d3f968746d3c75198142f2c07e
Sai kiran <saik82864@gmail.com>
2022-08-20 12:30:35 +0530

    prg-2

---
+++ Example.java
@@ -0,0 +1,11 @@
+public class Example {
+    public static void main(String args[]){
+        int a=15;
+        int b=10;
+        int sum=a+b;
+        System.out.println("Addition of two number="+sum);
+        
+
+    }
+    
+}
